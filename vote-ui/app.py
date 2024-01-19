09989296ed1c79c9873fbca90e7241b81591a6ec
poonamkadian <poonam.kadian@kpmg.co.uk>
2024-01-19 10:59:02 +0530

    module commented

--- vote-ui/app.py
+++ vote-ui/app.py
@@ -7,8 +7,11 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"Cat 🐺")
-option_b = os.getenv('OPTION_B', u"Dog 🐶")
+#option_a = os.getenv('OPTION_A', u"Cat 🐺")
+#option_b = os.getenv('OPTION_B', u"Dog 🐶")
+option_a = os.getenv('OPTION_A', "podman")
+option_b = os.getenv('OPTION_B', "docker")
+
 hostname = socket.gethostname()
 
 app = Flask(__name__)
